753d887f12bff54af96331c4e5f62540b9239b05
“chocomegane” <uky.choco@gmail.com>
2019-08-11 12:05:30 +0900

    Initial Commit

---
+++ jour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jour
+//
+//  Created by yuki.sawamura on 2019/08/11.
+//  Copyright © 2019年 yusawamu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
