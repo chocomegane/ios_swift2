44b8085752e32a17865a33f80b8bc380ba0d2fb9
“chocomegane” <uky.choco@gmail.com>
2019-09-22 11:34:16 +0900

    first commit

--- jour/ViewController.swift
+++ jour/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //　計算機の処理　start
 
+    // 数値が入る
+    @IBOutlet weak var filedA: UITextField!
+    // 数値が入る
+    @IBOutlet weak var filedB: UITextField!
+    // 結果が入る
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func celende(_ sender: Any) {
+        let resultA:Int? = Int(filedA.text!);
+        let resultB:Int? = Int(filedB.text!);
 
+        if(resultA != nil && resultB != nil ){
+            resultLabel.text = "答えは"+String(resultA!*resultB!);
+
+        }else{
+            resultLabel.text = "入力内容を再度ご確認ください"
+        }
+
+    }
+    
+    //　計算機の処理　end
+    
+    
+    //　配列処理　staet
+    
+    
+    // 戻るボタンと紐付け用としたが失敗
+    @IBAction func `return`(_ sender: Any) {
+//        同じコントローラの場合下記のものを使用してもろモーダルを閉じることはできない
+//        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
 }
 
---
+++ jour/ViewTwoController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewTwoController.swift
+//  jour
+//
+//  Created by yuki.sawamura on 2019/08/11.
+//  Copyright © 2019年 yusawamu. All rights reserved.
+//
+
+import UIKit
+
+class ViewTwoController: UIViewController {
+
+    @IBOutlet weak var wevView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func `return`(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
